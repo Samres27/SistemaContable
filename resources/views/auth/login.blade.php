1d8a3b4e72b023a7535f87a08a61302e23b6abaf
samuel navia <yosoyam4@samres27>
2025-03-13 09:51:13 -0400

    add: finish commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,36 @@
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <input type="text" name="name" placeholder="User">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-</form>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login BARUCK</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Agregar el CDN de Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+</head> 
+<body>   
+    <form method="POST" action="{{ route('login') }}" class="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
+        @csrf
+        <h2 class="text-2xl font-semibold text-center mb-6">Login</h2>
+
+        <div class="mb-4">
+            <label for="name" class="block text-sm font-medium text-gray-700">Username</label>
+            <input type="text" name="name" id="name" placeholder="Enter your username"
+                class="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+        </div>
+
+        <div class="mb-6">
+            <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password"
+                class="mt-2 w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+        </div>
+
+        <button type="submit"
+            class="w-full py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            Login
+        </button>
+
+        
+    </form>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/', function () {
     return redirect('/login');;
