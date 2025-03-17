9a290345141428bb3e4eaf76b8e0a4b46a64371b
samuel navia <yosoyam4@samres27>
2025-03-17 10:22:47 -0400

    add: comand

--- app/Console/Commands/CheckAndMigrate.php
+++ app/Console/Commands/CheckAndMigrate.php
@@ -1,30 +1,39 @@
 <?php
-
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
 
 class CheckAndMigrate extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:check-and-migrate';
+    protected $signature = 'check:migrate';
+    protected $description = 'Verifica si las tablas existen y ejecuta migraciones y seeders si es necesario';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        //
+        // Lista de tablas que deberían existir
+        $tablas = ['boletas', 'clientes','ventas','cobros','liquidaciones','descuentos','pagos','productos','proveedores']; 
+
+        $faltanTablas = false;
+
+        foreach ($tablas as $tabla) {
+            if (!Schema::hasTable($tabla)) {
+                $this->info("La tabla '$tabla' no existe. Ejecutando migraciones...");
+                $faltanTablas = true;
+                break; // Si una tabla falta, ya es suficiente para ejecutar las migraciones
+            }
+        }
+
+        if ($faltanTablas) {
+            $this->call('migrate');
+            $this->info('Migraciones ejecutadas.');
+
+            $this->call('db:seed', ['--class' => 'UserSeeder']);
+            $this->info('Seeders ejecutados.');
+        } else {
+            $this->info('Todas las tablas existen. No se requieren migraciones ni seeders.');
+        }
     }
 }
+
+
